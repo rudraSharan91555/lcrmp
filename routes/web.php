3150139f2fa5f990b3e5cdabb0bb28fece12f2b7
Rudra Sharan <rudrasharan45@gmail.com>
2024-12-07 18:02:29 +0530

    form creating using bootstarp

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,8 @@ Auth::routes();
 
 Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/logout', [App\Http\Controllers\HomeController::class, 'logout']);
+Route::get('/add-product',function(){
+    return view('admin.add-product');
+})->middleware('auth');
+
 // Route::get('/logout', 'HomeController@ logout');
